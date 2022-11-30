51ec2a5becd97d583b10a13eae6d7b5116013ca0
waseem <waseemattar14@gmail.com>
2022-11-30 10:45:02 +0530

    Creation

---
+++ main.py
@@ -0,0 +1,9 @@
+from flask import Flask, url_for, render_template, request, redirect
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
